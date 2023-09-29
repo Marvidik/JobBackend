7e59d1b55ad91d168b19ea5debe2516c46f16778
Marvidik <ebubeidika@gmail.com>
2023-09-29 17:38:18 +0100

    we just started

--- MyJobAPI/urls.py
+++ MyJobAPI/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Authentication.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'login/', LoginView.as_view(), name='knox_login'),
+    # path(r'logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
+    # path(r'logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
 ]
