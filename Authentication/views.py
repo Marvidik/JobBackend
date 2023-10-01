c7a27d9406750f368d5822e9662ae01ce78336d7
Marvidik <ebubeidika@gmail.com>
2023-10-01 15:40:35 +0100

    done with login and register view

--- Authentication/views.py
+++ Authentication/views.py
@@ -3,6 +3,12 @@ from django.contrib.auth import login
 from rest_framework import permissions
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.views import LoginView as KnoxLoginView
+from .serializer import UserSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView
+from knox.models import AuthToken
 
 class LoginView(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
@@ -13,3 +19,20 @@ class LoginView(KnoxLoginView):
         user = serializer.validated_data['user']
         login(request, user)
         return super(LoginView, self).post(request, format=None) 
+
+
+class RegistrationView(CreateAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        _, token = AuthToken.objects.create(user)
+        return Response(
+            {
+                "user": UserSerializer(user, context=self.get_serializer_context()).data,
+                "token": token,
+            },
+            status=status.HTTP_201_CREATED,
+        )
