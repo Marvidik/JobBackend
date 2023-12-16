9e10bc4555ce93637477b1bcf06a957d80f44bd4
Marvidik <ebubeidika@gmail.com>
2023-12-16 19:48:13 +0100

    made some minor changes

--- Authentication/views.py
+++ Authentication/views.py
@@ -12,7 +12,7 @@ from .serializer import JobsSerializer, UserSerializer
 def login(request):
     user=get_object_or_404(User,username=request.data['username'])
     if not user.check_password(request.data['password']):
-        return Response({"dtails":"Info Not Found"})
+        return Response({"details":"Info Not Found"})
     token,created=Token.objects.get_or_create(user=user)
     serializer=UserSerializer(instance=user)
     return Response({"token":token.key,"user":serializer.data})
