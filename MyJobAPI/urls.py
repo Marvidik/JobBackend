51015c63ac4e4d1e20628e0d140518ac593bbb45
unknown <ebubeidika@gmail.com>
2024-01-18 00:07:13 +0100

    adding test cases to the login and register APIS

--- MyJobAPI/urls.py
+++ MyJobAPI/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, re_path
-
 from Authentication import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('login',views.login),
-    re_path('signup',views.register),
+    re_path('login',views.login, name="login"),
+    re_path('signup',views.register,name="register"),
     re_path("jobs", views.jobs,name="all_jobs")
     
 ]
