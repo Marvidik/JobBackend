4c4c50273f7f11d255ee9279b39eb86d5a33afa9
unknown <ebubeidika@gmail.com>
2024-01-31 14:52:19 +0100

    a

--- Authentication/serializer.py
+++ Authentication/serializer.py
@@ -3,13 +3,14 @@ from rest_framework import serializers
 
 from .models import Jobs
 
-
+#  user serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = User
         fields = ( 'id','username', 'email', 'password')
 
 
+#  Jobs serializer 
 class JobsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Jobs  # Specify the model associated with the serializer
